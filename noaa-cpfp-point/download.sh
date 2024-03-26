e93adf160b2f576948394f8695e13036059bfcb5
Sanjog Thapa <sthapa@Thapa24-flt.local>
2024-03-26 16:53:21 -0500

    update dataset download script with new dataset and updated hierarchy. Also, tracked downloaded files

--- noaa-cpfp-point/download.sh
+++ noaa-cpfp-point/download.sh
@@ -6,9 +6,17 @@ wget https://gml.noaa.gov/aftp/data/trace_gases/co2/flask/surface/co2_surface-fl
 wget https://gml.noaa.gov/aftp/data/trace_gases/ch4/flask/surface/ch4_surface-flask_ccgg_text.zip
 wget https://gml.noaa.gov/aftp/data/trace_gases/ch4/pfp/surface/ch4_surface-pfp_ccgg_text.zip
 wget https://gml.noaa.gov/aftp/data/trace_gases/co2/pfp/surface/co2_surface-pfp_ccgg_text.zip
+wget https://gml.noaa.gov/aftp/data/trace_gases/co2/in-situ/surface/co2_surface-insitu_ccgg_text.zip
+wget https://gml.noaa.gov/aftp/data/trace_gases/co2/in-situ/tower/co2_tower-insitu_ccgg_text.zip
+wget https://gml.noaa.gov/aftp/data/trace_gases/ch4/in-situ/surface/ch4_surface-insitu_ccgg_text.zip
+wget https://gml.noaa.gov/aftp/data/trace_gases/ch4/in-situ/tower/ch4_tower-insitu_ccgg_text.zip
 
 # unzip to respective folders
-unzip -j -o co2_surface-flask_ccgg_text.zip co2_surface-flask_ccgg_text/*_event.txt -d noaa-cpfp-point/flask/co2
-unzip -j -o ch4_surface-flask_ccgg_text.zip ch4_surface-flask_ccgg_text/*_event.txt -d noaa-cpfp-point/flask/ch4
-unzip -j -o co2_surface-pfp_ccgg_text.zip co2_surface-pfp_ccgg_text/*_event.txt -d noaa-cpfp-point/surface-pfp/co2
-unzip -j -o ch4_surface-pfp_ccgg_text.zip ch4_surface-pfp_ccgg_text/*_event.txt -d noaa-cpfp-point/surface-pfp/ch4
+unzip -j -o co2_surface-flask_ccgg_text.zip co2_surface-flask_ccgg_text/*_event.txt -d co2/flask/surface
+unzip -j -o ch4_surface-flask_ccgg_text.zip ch4_surface-flask_ccgg_text/*_event.txt -d ch4/flask/surface
+unzip -j -o co2_surface-pfp_ccgg_text.zip co2_surface-pfp_ccgg_text/*_event.txt -d co2/pfp/surface
+unzip -j -o ch4_surface-pfp_ccgg_text.zip ch4_surface-pfp_ccgg_text/*_event.txt -d ch4/pfp/surface
+unzip -j -o co2_surface-insitu_ccgg_text.zip co2_surface-insitu_ccgg_text/*.txt -d co2/insitu/surface
+unzip -j -o co2_tower-insitu_ccgg_text.zip co2_tower-insitu_ccgg_text/*.txt -d co2/insitu/tower
+unzip -j -o ch4_surface-insitu_ccgg_text.zip ch4_surface-insitu_ccgg_text/*.txt -d ch4/insitu/surface
+unzip -j -o ch4_tower-insitu_ccgg_text.zip ch4_tower-insitu_ccgg_text/*.txt -d ch4/insitu/tower
