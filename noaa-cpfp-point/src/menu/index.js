3707edbea9f7554ef26a289d4eabad71a68839b6
Sanjog Thapa <sanzog03@gmail.com>
2024-04-22 11:27:18 -0500

    added menu module

---
+++ noaa-cpfp-point/src/menu/index.js
@@ -0,0 +1,42 @@
+import { CONTINUOUS, NON_CONTINIOUS, ALL } from "../enumeration";
+
+export const showOptions = (queryParams) => {
+    let publicUrl = process.env.PUBLIC_URL;
+    let {ghg, frequency, type} = queryParams;
+    //options for selection
+    let collectionMechanismDropdown = document.getElementById("collection-mechanism");
+
+    // if frequency is available only then show the button
+    // if frequency is continuous, change the dropdown selected value to continuous
+    // if frequency is non-continuous, change the dropdown selected value to non-continuous
+    if (!frequency) {
+        collectionMechanismDropdown.style.display = "none";
+        return;
+    }
+
+    if (frequency == CONTINUOUS) {
+        collectionMechanismDropdown.value = CONTINUOUS;
+    } else if (frequency == NON_CONTINIOUS){
+        collectionMechanismDropdown.value = NON_CONTINIOUS;    
+    } else {
+        collectionMechanismDropdown.value = ALL;
+    }
+    collectionMechanismDropdown.addEventListener("change", (e) => {
+        let clickedVal = e.target.value;
+        if (clickedVal == NON_CONTINIOUS & type != NON_CONTINIOUS) { // and previous is not continuous
+            let newlocation = `${publicUrl}/?ghg=${ghg}&frequency=${NON_CONTINIOUS}`;
+            window.location.href = newlocation;
+            collectionMechanismDropdown.value = NON_CONTINIOUS;
+        } else if (clickedVal == CONTINUOUS & type != CONTINUOUS) { // and previous is discrete
+            let newlocation = `${publicUrl}/?ghg=${ghg}&frequency=${CONTINUOUS}`;
+            window.location.href = newlocation;
+            collectionMechanismDropdown.value = CONTINUOUS;
+        } else if (clickedVal == ALL & type != ALL) { // and previous is discrete
+            let newlocation = `${publicUrl}/?ghg=${ghg}&frequency=${ALL}`;
+            window.location.href = newlocation;
+            collectionMechanismDropdown.value = ALL;} 
+        else {
+        // do nothing
+        }
+    });
+}
