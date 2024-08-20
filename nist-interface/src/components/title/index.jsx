5040543bca45f600eeedd7d3231f6d6e0596119f
Sanjog Thapa <sanzog03@gmail.com>
2024-08-20 15:44:08 -0500

    title uses full name for acronym when available. additional title styling changes.

--- nist-interface/src/components/title/index.jsx
+++ nist-interface/src/components/title/index.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import Typography from '@mui/material/Typography';
 
 import './index.css';
 
+const GHG_FULL_NAME = {
+    CO2: 'Carbon Dioxide',
+    CH4: 'Methane'
+};
+
 export function Title ({ ghg, agency }) {
+    let GHG = ghg.toUpperCase();
+    let GHGFullName = GHG;
+    if (GHG in GHG_FULL_NAME) {
+        GHGFullName = GHG_FULL_NAME[GHG];
+    }
+
     return (
         <div id="title">
-            <Typography variant='h5'>
-                {agency.toUpperCase()}: {ghg.toUpperCase()} Concentration Measurements
-            </Typography>
+            <strong>
+                {agency.toUpperCase()}: {GHGFullName} Concentration Measurements
+            </strong>
         </div>
     );
 }
