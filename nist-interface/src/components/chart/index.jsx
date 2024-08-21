3b977f178bf4f78556dd650b204e9b03cb05701d
Sanjog Thapa <sanzog03@gmail.com>
2024-08-21 14:42:59 -0500

    add chart help instructions

--- nist-interface/src/components/chart/index.jsx
+++ nist-interface/src/components/chart/index.jsx
@@ -2,7 +2,7 @@ import { Component, createRef } from 'react';
 import Chart from 'chart.js/auto';
 import Box from '@mui/material/Box';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faXmark, faRotateLeft } from '@fortawesome/free-solid-svg-icons';
+import { faXmark, faRotateLeft, faCircleInfo } from '@fortawesome/free-solid-svg-icons';
 
 import { plugin, options } from './helper';
 
@@ -146,9 +146,20 @@ export class ConcentrationChart extends Component {
     return (
       <Box sx={{height: "30em"}} id="chart-box">
           <div id="chart-container" className='fullSize'>
-            <div id="chart-controls">
-              <FontAwesomeIcon id="zoom-reset-button" icon={faRotateLeft} title="Reset Zoom" onClick={this.handleRefresh}/>
-              <FontAwesomeIcon id="chart-close-button" icon={faXmark} title="Close" onClick={this.handleClose}/>
+            <div id="chart-tools">
+              <div id="chart-instructions-container">
+              <div className="icon-and-instructions">
+                <FontAwesomeIcon icon={faCircleInfo} style={{margin: "12px"}}/>
+                <div id="chart-instructions">
+                  <p>1. Click and drag, scroll or pinch on the chart to zoom in.</p>
+                  <p>2. Click on the rectangle boxes on the side to toggle chart.</p>
+                </div>
+              </div>
+              </div>
+              <div id="chart-controls">
+                <FontAwesomeIcon id="zoom-reset-button" icon={faRotateLeft} title="Reset Zoom" onClick={this.handleRefresh}/>
+                <FontAwesomeIcon id="chart-close-button" icon={faXmark} title="Close" onClick={this.handleClose}/>
+              </div>
             </div>
             <canvas
               id = "chart"
