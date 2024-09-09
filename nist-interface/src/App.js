2b271d299d6fd7feb6bcb0726a6c401dec09bc2d
Paridhi Parajuli <ppanjuli@parajuli24-mbp.local>
2024-09-09 17:44:25 -0500

    change to BrowserRouter

--- nist-interface/src/App.js
+++ nist-interface/src/App.js
@@ -1,25 +1,19 @@
 import { Fragment } from 'react';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CssBaseline from '@mui/material/CssBaseline';
 import { DashboardContainer } from './pages/dashboardContainer';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <DashboardContainer />,
-  }
-]);
+import './App.css';
 
 function App() {
   return (
     <Fragment>
       <CssBaseline />
-      <RouterProvider router={router} />
+      <BrowserRouter basename={"/ghgcenter/custom-interfaces/nist-interface"}>
+        <Routes>
+          <Route path="/" element={<DashboardContainer />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </Fragment>
   );
 }
