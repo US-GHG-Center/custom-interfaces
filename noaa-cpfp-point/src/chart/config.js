d78c1e6fca01c53f9e56f4c4c77000fceffe2a41
Sanjog Thapa <sanzog03@gmail.com>
2024-10-03 14:21:49 -0500

    noaa chart to show UTC date time

--- noaa-cpfp-point/src/chart/config.js
+++ noaa-cpfp-point/src/chart/config.js
@@ -72,7 +72,7 @@ export const getOptions = (station, selectedGhg)=> ({
         x: {
         title: {
             display: true,
-            text: "Observation Date/Time",
+            text: "Observation Date/Time (UTC)",
             },
         grid: {
             display: false,
@@ -86,6 +86,11 @@ export const getOptions = (station, selectedGhg)=> ({
                 //   return "fadhsdsf"
                 // }
             },
+        adapters: {
+            date: {
+                zone: "utc"
+            }
+        }
         },
         y: {
         title: {
