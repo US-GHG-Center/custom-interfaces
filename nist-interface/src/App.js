78fabe59288d0f461a44c5bafdbd7cc584cf7a54
Paridhi Parajuli <ppanjuli@parajuli24-mbp.local>
2024-10-04 10:16:06 -0500

    test different basename

--- nist-interface/src/App.js
+++ nist-interface/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <Fragment>
       <CssBaseline />
-      <BrowserRouter basename={BASE_PATH}>
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<DashboardContainer />}>
           </Route>
