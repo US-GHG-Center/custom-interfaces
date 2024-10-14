f019a1f368c34c82abe8fa88df9494e98ae46e41
anisbhsl <bhusal.anish12@gmail.com>
2024-10-14 10:47:22 -0500

    refactored info sidebar component

--- urban-dashboard/src/components/infoSidebar/index.js
+++ urban-dashboard/src/components/infoSidebar/index.js
@@ -9,6 +9,71 @@ import { ColorMapChart } from '../cards/colorMapChart';
 
 import "./index.css";
 
+function DataSnippet({ dataset }) {
+    let briefSnippet = "data is available for the contiguous United States. Locations indicated are for representative purposes only, based on the top 30 U.S. cities by population size, according to the 2021 U.S. Census Bureau figures and city “Place” boundaries.";
+    if (dataset === "vulcan") {
+        briefSnippet = `Vulcan ${briefSnippet}`
+    } else if (dataset === "gra2pes") {
+        briefSnippet = `GRA²PES ${briefSnippet}`
+    }
+
+    return (
+        <>
+            <Typography style={{ fontSize: '12px', color: '#1E1E1E', fontFamily: "Inter", marginTop: "5px" }}>
+                {briefSnippet}
+            </Typography>
+        </>
+    )
+}
+
+// TitlePopulationAndArea is a wrapper for Title, Data Snippet and Population Cards
+function TitlePopulationAndArea({
+    urbanRegions,
+    selection,
+    setSelection,
+    handleZoomOut,
+    dataset
+}) {
+    return (
+        <div>
+            <Title
+                selection={selection}
+                setSelection={setSelection}
+                handleZoomOut={handleZoomOut}
+            />
+
+            {/* Data Snippet Card */}
+            {!selection && <DataSnippet dataset={dataset} />}
+
+            {/* Population and Area Card */}
+            {selection && (
+                <>
+                    <PopulationCard selection={selection} urbanRegions={urbanRegions} />
+                </>
+            )}
+        </div>
+    )
+}
+
+
+// DataInsights plots charts on the info sidebar
+function DataInsights({ dataset, selection }) {
+    return (
+        <>
+            {selection && (
+                <>
+                    {/* Secondary separator line */}
+                    <div className="info-border-secondary" />
+                    <DataInsightsCard
+                        dataset={dataset}
+                        selection={selection}
+                    />
+                </>
+            )}
+        </>
+    )
+}
+
 
 export function InfoSidebar({
     urbanRegions,
@@ -18,66 +83,29 @@ export function InfoSidebar({
     dataset
 }) {
 
-    let briefSnippet = "data is available for the contiguous United States. Locations indicated are for representative purposes only, based on the top 30 U.S. cities by population size, according to the 2021 U.S. Census Bureau figures and city “Place” boundaries.";
-    if (dataset === "vulcan") {
-        briefSnippet = `Vulcan ${briefSnippet}`
-    } else if (dataset === "gra2pes") {
-        briefSnippet = `GRA²PES ${briefSnippet}`
-    }
-
     return (
         <div>
             <div className={"info-sidebar " + (selection ? 'info-sidebar-full-height' : '')}>
-                <div>
-                    <Title
-                        selection={selection}
-                        setSelection={setSelection}
-                        handleZoomOut={handleZoomOut}
-                    />
-
-                    {/* Data Snippet Card */}
-
-                    {!selection && (
-                        <>
-                            <Typography style={{ fontSize: '12px', color: '#1E1E1E', fontFamily: "Inter", marginTop: "5px" }}>
-                                {briefSnippet}
-                            </Typography>
-                        </>
-                    )}
-
-                    {/* Population and Area Card */}
-                    {selection && (
-                        <>
-                            <PopulationCard selection={selection} urbanRegions={urbanRegions} />
-                        </>
-                    )}
-                </div>
+                {/* Title, Data Snippet, Population & Area */}
+                <TitlePopulationAndArea
+                    urbanRegions={urbanRegions}
+                    selection={selection}
+                    setSelection={setSelection}
+                    handleZoomOut={handleZoomOut}
+                    dataset={dataset} />
 
                 {/* Primary separator line */}
                 <div className="info-border-primary" />
-
 
                 {/* Dataset Card */}
                 <DatasetCard dataset={dataset} />
 
                 {/* Dataset Insights Card */}
-
-                {selection && (
-                    <>
-                        {/* Secondary separator line */}
-                        <div className="info-border-secondary" />
-                        <DataInsightsCard
-                            dataset={dataset}
-                            selection={selection}
-                        />
-                    </>
-                )}
+                <DataInsights dataset={dataset} selection={selection} />
 
             </div>
 
             <ColorMapChart dataset={dataset} />
-
-
         </div>
     )
 }
