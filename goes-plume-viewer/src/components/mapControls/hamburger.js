f49c9377fc7cd335e121a5d921dbfa69d051d63b
Sanjog Thapa <sanzog03@gmail.com>
2024-11-14 14:30:28 -0600

    added hamburger icon

--- goes-plume-viewer/src/components/mapControls/hamburger.js
+++ goes-plume-viewer/src/components/mapControls/hamburger.js
@@ -1,18 +1,40 @@
+import IconButton from "@mui/material/IconButton";
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+import ReactDOM from "react-dom/client";
+
+function HamburgerIcon() {
+  return (
+    <IconButton className="menu-open-icon" >
+     <MenuOpenIcon/>
+    </IconButton>
+  );
+}
+
 export class HamburgerControl {
   constructor(onClick) {
     this._onClick = onClick;
+    this.root = null;
+    this.isMounted = true;
+  }
+
+  onClick() {
+    if (!this.isMounted) return;
   }
 
   onAdd(map) {
     this._map = map;
     this._container = document.createElement('div');
     this._container.className = 'mapboxgl-ctrl mapboxgl-ctrl-group';
-    this._container.innerHTML = '<button><i class="fa-solid fa-bars"></i></button>';
+    const root = ReactDOM.createRoot(this._container);
+    root.render(<HamburgerIcon/>);
+    this.root = root;
     this._container.onclick = this._onClick;
     return this._container;
   }
 
   onRemove() {
+    this.isMounted = false;
+    this.root.unmount();
     this._container.parentNode.removeChild(this._container);
     this._map = undefined;
   }
