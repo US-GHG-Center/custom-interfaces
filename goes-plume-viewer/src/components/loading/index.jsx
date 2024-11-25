f7f35295200a7654206b1895da8a1fe7ac418487
Sanjog Thapa <sanzog03@gmail.com>
2024-11-25 09:46:32 -0600

    when loading, no other components should be clickable

--- goes-plume-viewer/src/components/loading/index.jsx
+++ goes-plume-viewer/src/components/loading/index.jsx
@@ -14,7 +14,7 @@ export function LoadingSpinner() {
                   left: 0,
                   width: "100%",
                   height: "100%",
-                  zIndex: 1000,
+                  zIndex: 2000,
                 }}
               />
 }
