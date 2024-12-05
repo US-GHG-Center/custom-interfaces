dc0b1b2b361d777cbb5005c1fe2327f83577158f
Abdelhak Marouane <am0089@uah.edu>
2024-12-05 13:44:01 -0600

    Fixing conflict

--- emit-ch4plume-v1/src/helper.js
+++ emit-ch4plume-v1/src/helper.js
@@ -1,5 +1,21 @@
-const d3 = require("d3");
+// It has no global vars referenced
+function filterByDates(data, sDate, eDate) {
+      const start = new Date(sDate);
+      const end = new Date(eDate);
+      // Filter geojson features based on the UTC Time Observed
+      const filteredFeatures = data.features.filter(feature => {
+          const observedTime = new Date(feature.properties["UTC Time Observed"]);
+          // Check if the observed time falls within the given date range
+          return (observedTime >= start && observedTime <= end);
+      });
+      return {
+          ...data,
+          features: filteredFeatures
+      };
 
+}
+
+const d3 = require("d3");
 function generateScale(min, max, step) {
   const numbers = [];
   for (let i = min; i <= max; i += step) {
@@ -8,45 +24,6 @@ function generateScale(min, max, step) {
   numbers[numbers.length - 1] += "+";
   return numbers;
 }
-
-
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-
-  elmnt.onmousedown = dragMouseDown;
-
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
-
 
 function createColorbar(VMIN, VMAX) {
 
@@ -58,20 +35,29 @@ function createColorbar(VMIN, VMAX) {
   // Create a colorbar element
   const colorbar = d3.select("body").append("div").attr("class", "colorbar");
 
-  colorbar
+  // Define colorbar width and height
+  const colorbarWidth = 350; // Adjusted width
+  const colorbarHeight = 12; // Height of the colorbar
+  const numSegments = 100; // Number of color segments
+
+
+  // Create the SVG container for the colorbar
+  const svg = colorbar
     .append("svg")
-    .attr("width", 300) // Adjust the width as needed
-    .attr("height", 12) // Adjust the height as needed
-    .attr("rx", 10)
+    .attr("width", colorbarWidth)
+    .attr("height", colorbarHeight) // Set the height of the colorbar
+    .attr("rx", 10);
+
+  // Add color segments (rectangles)
+  svg
     .append("g")
     .selectAll("rect")
-    .data(d3.range(VMIN, VMAX, (VMAX - VMIN) / 100)) // Adjust the number of color segments as needed
+    .data(d3.range(VMIN, VMAX, (VMAX - VMIN) / numSegments)) // Number of segments
     .enter()
     .append("rect")
-    .attr("height", 20)
-    .attr("width", 3) // Adjust the width of each color segment
-
-    .attr("x", (d, i) => i * 3)
+    .attr("height", colorbarHeight) // Match the height of the colorbar
+    .attr("width", colorbarWidth / numSegments) // Dynamically calculate width of each segment
+    .attr("x", (d, i) => (i * (colorbarWidth / numSegments)) ) // Shift by 25px to center the rects
     .attr("fill", (d) => colorScale(d));
 
   // Define custom scale labels
@@ -89,6 +75,7 @@ function createColorbar(VMIN, VMAX) {
     .enter()
     .append("div")
     .attr("class", "colorbar-scale-label-horizontal")
+    .style("text-align","left")
     .text((d) => d); // Set the label text
 
   // Add the label "Methane enhancement (ppm m)" under the scale labels
@@ -102,100 +89,156 @@ function createColorbar(VMIN, VMAX) {
   // Add CSS styles to position and style the colorbar
   colorbar
     .style("position", "absolute")
-    .style("bottom", "60px") // Adjust the top position as needed
-    .style("right", "50px") // Adjust the left position as needed
+    .style("bottom", "20px") // Adjust the top position as needed
+    .style("right", "10px") // Adjust the left position as needed
     .style("background-color", "white")
     .style("padding", "12px");
 
   // Add CSS styles to style horizontal scale labels
   scaleLabelContainer
     .style("display", "flex")
-    .style("justify-content", "space-between")
+    .style("justify-content", "center")
     .style("margin-bottom", "12px"); // Adjust margin as needed
-  const dateRange = d3.select("body").append("div").attr("class", "date-range");
-
-  const dateRangeContainer = dateRange
-    .append("p")
-    .attr("class", "scale-label-container");
-  dateRangeContainer.append("label").attr("for", "amount").text("Date range:");
-  dateRangeContainer
-    .append("input")
-    .attr("type", "text")
-    .attr("id", "amount")
-    .style("border", "0")
-    .style("color", "#C85964")
-    .style("font-weight", "bold")
-    .style("size", "100")
-    .style("width", "240px");
-  dateRange.append("div").attr("id", "slider-range");
-
-  dateRange
-    .style("position", "absolute")
-    .style("top", "60px") // Adjust the top position as needed
-    .style("left", "50px") // Adjust the left position as needed
-    .style("background-color", "white");
 }
 
-const getFilename = function (pathStr) {
-  return pathStr.substring(pathStr.lastIndexOf("/") + 1);
-};
+function addTimelineMarkers(utcTimesObserved, start_date, end_date, color, zindex, height,width, radius) {
+  const slider = document.querySelector('.mapboxgl-ctrl-timeline__slider');
+  const sliderRect = slider.getBoundingClientRect();
+  const parentRect = slider.parentNode.getBoundingClientRect();
+  const existingMarkers = slider.parentNode.querySelectorAll('.timeline-marker');
+  existingMarkers.forEach(marker => marker.remove());
+  const offsetLeftStart = sliderRect.left - parentRect.left;
+  slider.parentNode.style.position = 'relative';
 
-function displayPropertiesWithD3(properties) {
+  // Function to create a marker
+  function createMarker(color, leftOffset) {
+      const marker = document.createElement('div');
+      marker.style.position = 'absolute';
+      marker.style.width = `${width}px`;
+      marker.style.zIndex= zindex
+      marker.style.height = `${height}px`;
+      marker.style.opacity = '1';
+      marker.style.borderRadius = `${radius}%`;
+      marker.style.backgroundColor = color;
+      marker.style.left = `${leftOffset}px`;
+      marker.style.top = `${slider.offsetTop - (height/2) +2 }px`;
+      slider.parentNode.appendChild(marker);
+  }
+  const startTime = new Date(start_date).getTime();
+  const endTime = new Date(end_date).getTime();
+  const totalDuration = endTime - startTime;
 
-  const important_keys = [
-    "Max Plume Concentration (ppm m)"
-  ]
+  utcTimesObserved.forEach(timeObserved => {
+      const observedTime = new Date(timeObserved).getTime();
+      const relativePosition = (observedTime - startTime) / totalDuration;
+      const observedOffsetLeft = offsetLeftStart + (relativePosition * sliderRect.width);
+      createMarker(color, observedOffsetLeft);
+  });
+}
 
+function beforeAnimation(map){
+  map.dragPan.disable();
+  map.scrollZoom.disable();
+  map.boxZoom.disable();
 
-  const combinedList = important_keys.concat(Object.keys(properties));
+  const {s:start_date, e:end_date} = getSliderValues();
+  const cov = document.getElementById("showCoverage").checked;
+  preservedState = {
+      dates: {
+          startDate:start_date,
+          endDate: end_date
+      },
+      coverage: cov,
+  }
+  $("#slider-range").slider("disable");
+  $("#slider-range").css("opacity", "0.4"); 
+  $("#amount").css("opacity", 0); 
+  document.querySelector('.mapboxgl-ctrl button.mapboxgl-ctrl-zoom-in').disabled = true;
+  document.querySelector('.mapboxgl-ctrl button.mapboxgl-ctrl-zoom-out').disabled = true;
+  document.getElementById('refresh').disabled = true;
+  document.querySelector('.autocomplete-search-box .search-box').disabled = true;
+  document.getElementById('showCoverage').disabled = true;
 
-  const new_sorted_properties = [...new Set(combinedList)];
-  // Create a display_div element
+  document.querySelector('.autocomplete-search-box .search-box').style.opacity = '0.4'
+  document.querySelector('.slider').style.opacity = '0.4';
 
-  // Create an HTML string to display the properties
-  let html =
-    "<span id=\"close\" onclick=\"document.getElementById('display_props').style.display='none'\" >x</span><table>";
+  document.querySelectorAll('input[type="datetime-local"]').forEach(input => {
+      input.disabled = true;
+      input.style.opacity = '0.4';
+  });
+  return {start_date, end_date, cov}
+}
+
+function afterAnimation(map, preservedState){
+  map.dragPan.enable();
+  map.scrollZoom.enable();
+  map.boxZoom.enable();
+
+  $("#slider-range").slider("enable");
+  $("#slider-range").css("opacity", "1"); 
+  const sDate= new Date(preservedState.startDate).getTime()/1000;
+  const eDate= new Date(preservedState.endDate).getTime()/1000;
+  $("#slider-range").slider("values", [sDate, eDate]);
+  $("#amount").css("opacity", 1); 
+  document.querySelector('.mapboxgl-ctrl button.mapboxgl-ctrl-zoom-in').disabled = false;
+  document.querySelector('.mapboxgl-ctrl button.mapboxgl-ctrl-zoom-out').disabled = false;
+  document.getElementById('refresh').disabled = false;
+  document.querySelector('.autocomplete-search-box .search-box').disabled = false;
+  document.getElementById('showCoverage').disabled = false;
+
+  document.querySelector('.autocomplete-search-box .search-box').style.opacity = '1'
+  document.querySelector('.slider').style.opacity = '1';
+
+  // Restore dates and coverage
+  document.getElementById("showCoverage").checked = preservedState.coverage;
+}
+
+function isFeatureWithinBounds(feature, bounds) {
+  if (feature.geometry.type !== 'Polygon') return false;
+
+  // Create a bounding box feature from the map bounds
+  const boundingBox = turf.bboxPolygon([
+    bounds._sw.lng,
+    bounds._sw.lat,
+    bounds._ne.lng,
+    bounds._ne.lat
+  ]);
+
+  // Check if the feature intersects with the bounding box
+  return turf.booleanIntersects(feature.geometry, boundingBox);
+}
+
+function formatTimestampToDate(timestamp) {
+  const d = new Date(timestamp);  // Convert the timestamp to a Date object
   
+  const year = d.getUTCFullYear();  // Get the full year in UTC
+  const month = String(d.getUTCMonth() + 1).padStart(2, '0');  // Get the month (0-indexed, so add 1)
+  const day = String(d.getUTCDate()).padStart(2, '0');  // Get the day of the month
+  const hours = String(d.getUTCHours()).padStart(2, '0');  // Get the hour (UTC)
+  const minutes = String(d.getUTCMinutes()).padStart(2, '0');  // Get the minutes (UTC)
 
-  const keys_to_exclude = [
-    "id",
-    "SceneFID",
-    "map_endtime",
-    "Scene FIDs",
-    "style",
-    "DCID",
-    "DAAC Scene Numbers",
-    "plume_complex_count",
-  ];
+  return `${year}-${month}-${day}T${hours}:${minutes}`;  // Return in the desired format
+}
 
-   // Iterate through the properties and create list items
-  new_sorted_properties.forEach(key => {
-    if (!keys_to_exclude.includes(key)) {
 
-      value = properties[key];
-      if (value.toString().startsWith("https://")) {
-        value = `<a href="${value}" target="_blank" >${getFilename(value)} <svg width="12" height="12" fill="currentColor" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 16 16" aria-hidden="true" class="expand-top-right__CollecticonExpandTopRight-sc-1bjhv94-0"><title>expand top right icon</title><path d="M3,5h4V3H1v12h12V9h-2v4H3V5z M16,8V0L8,0v2h4.587L6.294,8.294l1.413,1.413L14,3.413V8H16z"></path></svg></a>`;
-      }
-      html += `<tr><td><strong>${key}:</strong></td><td>${value}</td></tr>`;
-
-    }
-  })
-  html += "</table>";
-
-  d3.select("body").select("#display_props").remove();
-
-  const display_div = d3
-    .select("body")
-    .append("div")
-    .attr("id", "display_props")
-    .html(html);
-
-  // Add CSS styles to position and style the display_div
-  display_div.style("position", "absolute");
+function getSliderValues() {
+  const dateSlider = document.getElementById("slider-range");
+  if (dateSlider) {
+    const s= formatTimestampToDate($("#slider-range").slider("values", 0) * 1000)
+    const e= formatTimestampToDate($("#slider-range").slider("values", 1) * 1000)
+    return {s,e}
+  } else {
+    console.log("Slider is not initialized yet.");
+  }
 }
 
 module.exports = {
-  createColorbar: createColorbar,
-  displayPropertiesWithD3: displayPropertiesWithD3,
-  dragElement: dragElement
-};
+    filterByDates: filterByDates,
+    createColorbar: createColorbar,
+    addTimelineMarkers: addTimelineMarkers,
+    afterAnimation: afterAnimation,
+    beforeAnimation: beforeAnimation,
+    isFeatureWithinBounds: isFeatureWithinBounds,
+    getSliderValues: getSliderValues,
+    formatTimestampToDate: formatTimestampToDate
+  };
---
+++ emit-ch4plume-v1/src/measure-tool-helper.js
@@ -0,0 +1,311 @@
+const measureVariables = {
+  measureToggled: false,
+  scale: "miles",
+  scaleText: "mi",
+};
+
+function toggleMeasureIcon(measureToggled) {
+  if (measureToggled) {
+    $("#measure-icon i")
+      .removeClass("fa-ruler-horizontal")
+      .addClass("fa-arrows-left-right-to-line");
+  } else {
+    $("#measure-icon i")
+      .removeClass("fa-arrows-left-right-to-line")
+      .addClass("fa-ruler-horizontal");
+  }
+}
+
+// GeoJSON object to hold  measurement features
+const distancePoints = {
+  type: "FeatureCollection",
+  features: [],
+};
+const measureLine = {
+  type: "FeatureCollection",
+  features: [],
+};
+//GeoJson object to hold distance label
+const distanceLabel = {
+  type: "FeatureCollection",
+  features: [],
+};
+
+// Used to draw a line between points
+const linestring = {
+  type: "Feature",
+  geometry: {
+    type: "LineString",
+    coordinates: [],
+  },
+};
+const pointLayer = {
+  id: "measure-points",
+  type: "circle",
+  source: "distancePoints",
+  paint: {
+    "circle-radius": 4.5,
+    "circle-color": "#00BFFF",
+  },
+  filter: ["in", "$type", "Point"],
+};
+const labelLayer = {
+  id: "measure-label",
+  type: "symbol",
+  source: "distanceLabel",
+  layout: {
+    "text-field": ["get", "description"],
+    "text-font": ["Open Sans Bold", "Arial Unicode MS Bold"],
+    "text-size": 13,
+    "text-letter-spacing": 0.1,
+    "text-justify": "center",
+    "symbol-spacing": 1000,
+    "text-offset": [0, 1.5],
+    "text-anchor": "bottom",
+  },
+  paint: {
+    "text-color": "#fff",
+    "text-halo-color": "#000",
+    "text-halo-width": 2,
+  },
+};
+
+const lineLayer = {
+  id: "measure-line",
+  type: "line",
+  source: "measureLine",
+  layout: {
+    "line-cap": "round",
+    "line-join": "round",
+  },
+  paint: {
+    "line-color": "#00BFFF",
+    "line-width": 2,
+  },
+  filter: ["in", "$type", "LineString"],
+};
+
+//distance label Symbol
+const distanceLabelAnchor = {
+  type: "Feature",
+  properties: {
+    description: "",
+  },
+  geometry: {
+    type: "LineString",
+    coordinates: [],
+  },
+};
+
+const onClearDistanceClick = (map) => {
+  distancePoints.features.length = 0;
+  measureLine.features.length = 0;
+  distanceLabel.features.length = 0;
+  map.getSource("distanceLabel").setData(distanceLabel);
+  map.getSource("measureLine").setData(distanceLabel);
+  map.getSource("distancePoints").setData(distancePoints);
+  $("#clear-icon-main").removeClass("clicked");
+};
+
+function changeCursor(map) {
+  const totalPoints = distancePoints.features.filter(
+    (f) => f.geometry.type === "Point"
+  );
+  // Change the cursor to a pointer when hovering over a point on the map.
+  // Otherwise cursor is a crosshair.
+  const crosshair = totalPoints.length < 2 && measureVariables.measureToggled;
+  map.getCanvas().style.cursor = crosshair ? "crosshair" : "pointer";
+}
+
+function addMeasurementAnchor(e, map) {
+  const features = map.queryRenderedFeatures(e.point, {
+    layers: ["measure-points"],
+  });
+  const totalPoints = distancePoints.features.filter(
+    (f) => f.geometry.type === "Point"
+  );
+  // If a feature was clicked, remove it from the map.
+  if (features.length) {
+    const id = features[0].properties.id;
+    distancePoints.features = distancePoints.features.filter(
+      (point) => point.properties.id !== id
+    );
+    measureLine.features.length = 0;
+  }
+  if (totalPoints.length == 1 && features.length == 0) {
+    distancePoints.features.length = 0;
+    measureLine.features.length = 0;
+    distanceLabel.features.length = 0;
+  }
+  if (totalPoints.length == 0 && features.length == 0) {
+    const point = {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [e.lngLat.lng, e.lngLat.lat],
+      },
+      properties: {
+        id: String(new Date().getTime()),
+      },
+    };
+    distancePoints.features.push(point);
+  }
+
+  if (distancePoints.features.length > 0) {
+    $("#clear-icon-main").addClass("clicked");
+  }
+  if (distancePoints.features.length == 0) {
+    $("#clear-icon-main").removeClass("clicked");
+  }
+  map.getSource("distanceLabel").setData(distanceLabel);
+  map.getSource("distancePoints").setData(distancePoints);
+  map.getSource("measureLine").setData(measureLine);
+  map.moveLayer("measure-points");
+  map.moveLayer("measure-label");
+}
+
+function disableMeasurementMode(map) {
+  toggleMeasureIcon(measureVariables.measureToggled);
+  onClearDistanceClick(map);
+  $("#display_props").removeClass("hidden");
+  $("#plume-id-search-box").removeClass("disabled");
+  $(".date-range").removeClass("disabled");
+  measureVariables.measureToggled = !measureVariables.measureToggled;
+  changeCursor(map);
+}
+
+function addMeasurementSource(map) {
+  map.addSource("distancePoints", {
+    type: "geojson",
+    data: distancePoints,
+  });
+  map.addSource("measureLine", {
+    type: "geojson",
+    data: measureLine,
+  });
+  map.addSource("distanceLabel", {
+    type: "geojson",
+    data: distanceLabel,
+  });
+}
+
+function addMeasurementLayer(map) {
+  map.addLayer(pointLayer);
+  map.addLayer(labelLayer);
+  map.addLayer(lineLayer);
+}
+
+function addMeasurementControls(map) {
+  map.addControl(new MeasureDistance(map));
+}
+
+function addClearControl(map) {
+  map.addControl(new ClearDistancePoints(map));
+}
+
+function createMeasuringLine(e, map) {
+  measureLine.features.pop();
+  const anchorPoint = distancePoints.features[0];
+  const startCoordinates = anchorPoint.geometry.coordinates;
+  const endCoordinates = [e.lngLat.lng, e.lngLat.lat];
+  linestring.geometry.coordinates = [startCoordinates, endCoordinates];
+  distanceLabelAnchor.geometry.coordinates = [endCoordinates, startCoordinates];
+  // const value = document.createElement("pre");
+  const turfUnits = measureVariables.scale === "miles" ? "miles" : "kilometers";
+  const distance = turf.length(linestring, {
+    units: turfUnits,
+  });
+  const labelUnit = measureVariables.scale === "miles" ? " miles" : " km";
+  distanceLabelAnchor.properties.description = `${distance.toFixed(
+    2
+  )} ${labelUnit}`;
+  distanceLabelAnchor.properties.icon = `${distance.toFixed(2)} ${labelUnit}`;
+  measureLine.features.push(linestring);
+  distanceLabel.features.push(distanceLabelAnchor);
+  map.getSource("measureLine").setData(measureLine);
+  map.getSource("distanceLabel").setData(distanceLabel);
+  map.moveLayer("measure-line");
+  map.moveLayer("measure-label");
+}
+class MapControls {
+  onRemove() {
+    this.container.parentNode.removeChild(this.container);
+    this.map = undefined;
+  }
+}
+class MeasureDistance extends MapControls {
+  constructor(map) {
+    super();
+    this.onClick = this.onClick.bind(this);
+    this.map = map;
+  }
+  onClick() {
+    toggleMeasureIcon(measureVariables.measureToggled);
+
+    measureVariables.measureToggled = !measureVariables.measureToggled;
+    $("#display_props").toggleClass("hidden");
+    $("#plume-id-search-box").toggleClass("disabled");
+    $(".date-range").toggleClass("disabled");
+
+    changeCursor(this.map);
+    this.map.doubleClickZoom.disable();
+  }
+  onAdd(map) {
+    this.map = map;
+    this.container = document.createElement("div");
+    this.container.className = "mapboxgl-ctrl mapboxgl-ctrl-group";
+    this.container.addEventListener("contextmenu", (e) => e.preventDefault());
+    this.container.addEventListener("click", (e) => this.onClick());
+    this.container.innerHTML =
+      '<div id="measure-icon-main">' +
+      "<button>" +
+      '<span id="measure-icon" class="mapboxgl-ctrl-icon" aria-hidden="true" title="Measure Tool">' +
+      '<i class="fa-solid fa-arrows-left-right-to-line"></i>' +
+      "</span>" +
+      "</button>" +
+      "</div>";
+    return this.container;
+  }
+}
+class ClearDistancePoints extends MapControls {
+  constructor(map) {
+    super();
+    this.onClick = this.onClick.bind(this);
+    this.map = map;
+  }
+  onClick() {
+    onClearDistanceClick(this.map);
+  }
+  onAdd(map) {
+    this.map = map;
+    this.container = document.createElement("div");
+    this.container.className = "mapboxgl-ctrl mapboxgl-ctrl-group";
+    this.container.addEventListener("contextmenu", (e) => e.preventDefault());
+    this.container.addEventListener("click", (e) => this.onClick());
+    this.container.innerHTML =
+      '<div id="clear-icon-main">' +
+      "<button>" +
+      '<span id="clear-icon" class="mapboxgl-ctrl-icon" aria-hidden="true" title="Measure Tool">' +
+      '<i class="fa-solid fa-eraser"></i>' +
+      "</span>" +
+      "</button>" +
+      "</div>";
+    return this.container;
+  }
+}
+
+module.exports = {
+  //variables
+  distancePoints: distancePoints,
+  measureVariables: measureVariables,
+  //methods
+  addMeasurementAnchor: addMeasurementAnchor,
+  disableMeasurementMode: disableMeasurementMode,
+  createMeasuringLine: createMeasuringLine,
+  addMeasurementSource: addMeasurementSource,
+  addMeasurementLayer: addMeasurementLayer,
+  addMeasurementControls: addMeasurementControls,
+  addClearControl: addClearControl,
+  //classes
+  MapControls: MapControls,
+};
