9498b15f2a0b395b86ddd88f25792ffd59b50036
Sanjog Thapa <sanzog03@gmail.com>
2025-08-08 14:43:42 -0500

    change the divider color to be grey

--- goes-plume-viewer/src/components/title/index.jsx
+++ goes-plume-viewer/src/components/title/index.jsx
@@ -22,13 +22,7 @@ export const Title = ({ children }) => {
             >
                 The Geostationary Operational Environmental Satellites collect images of the surface every 5 minutes. Only very large emission events can be detected, but plume expansion is easy to see over time. More plumes will be added soon.
             </Typography>
-            <Divider
-                sx={{
-                    borderColor: "var(--main-blue)",
-                    borderWidth: "1px",
-                    margin: "0.3rem 1.4rem",
-                }}
-            />
+            <Divider className='title-divider'/>
             <div className="title-content">
                 { children }
             </div>
