a01072a14e235c68e177ff706ef01a49dbdd6305
Sandesh Pandey <sandeshsinbox@gmail.com>
2025-09-18 11:42:35 -0500

    Display No data available when chart data is not present

--- nist-interface/src/components/chart/concentrationChart.jsx
+++ nist-interface/src/components/chart/concentrationChart.jsx
@@ -271,11 +271,29 @@ export class ConcentrationChart extends Component {
               </div>
             </div>
           </div>
-          {this.state.chartDataIsLoading && <LoadingSpinner />}
+          {this.state.chartDataIsLoading ? (
+            <LoadingSpinner />
+          ) : this.chart && this.chart.data.datasets[0].data.length === 0 ? (
+            <div style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100%',
+              width: '100%',
+              fontSize: '18px',
+              color: '#666'
+            }}>
+              No data available
+            </div>
+          ) : null}
           <canvas
             id="chart"
             className="fullWidth"
-            style={{ width: "100%", height: "100%" }}
+            style={{
+              width: "100%",
+              height: "100%",
+              display: this.chart && this.chart.data.datasets[0].data.length === 0 && !this.state.chartDataIsLoading ? 'none' : 'block'
+            }}
             ref={(chartCanvas) => (this.chartCanvas = chartCanvas)}
           />
         </div>
