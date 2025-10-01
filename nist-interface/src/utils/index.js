806bf230c23c6d9c42718d25bfa07a7cbce3ac78
Ram Sharan Rimal <rsr0012@uah.edu>
2025-10-01 11:57:25 -0500

    fix classnames for marker

--- nist-interface/src/utils/index.js
+++ nist-interface/src/utils/index.js
@@ -9,9 +9,9 @@ export const getMarkerColor = (index) => {
 export const getMarkerSVG = (color, strokeColor="#000000") => {
     return `
     <svg fill="${color}" width="30px" height="30px" viewBox="-51.2 -51.2 614.40 614.40" xmlns="http://www.w3.org/2000/svg">
-        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="${strokeColor}"
-            stroke-width="10.24">
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" strokeLinejoin="round" stroke="${strokeColor}"
+            strokeWidth="10.24">
             <path
                 d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path>
         </g>
@@ -25,9 +25,9 @@ export const getMarkerSVG = (color, strokeColor="#000000") => {
 export const getMarkerSVGComponent = (color, strokeColor="#000000") => {
     return (
         <svg fill={color} width="30px" height="30px" viewBox="-51.2 -51.2 614.40 614.40" xmlns="http://www.w3.org/2000/svg">
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke={strokeColor}
-                stroke-width="10.24">
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke={strokeColor}
+                strokeWidth="10.24">
                 <path
                     d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path>
             </g>
